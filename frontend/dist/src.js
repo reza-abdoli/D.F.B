dcfae8524dc09f5d3190be9d5c149ee98c332e2f
reza <rabdoli2002@gmail.com>
2023-12-08 04:48:57 +0330

    go done

--- frontend/dist/src.js
+++ frontend/dist/src.js
@@ -33,32 +33,61 @@ goTextBtn = document.getElementById('goTextBtn')
 gosh256Btn = document.getElementById('goSha256Btn')
 
 
-let goUrl = "http://localhost:3061/go/sha256";
+
 
 goTextBtn.addEventListener('click', () => {
+    let goUrl = "http://localhost:3061/go/sha256";
     let input = document.getElementById('goInput').value;
-    if (input.length < 8) {
-     //   document.getElementById('goInputStatus').innerHTML = "Less than 8 chars";
-     //   return ;
-    }
+    // if (input.length < 8) {
+    //     document.getElementById('goShaStatus').innerHTML = "Less than 8 chars";
+    //     alert(11)
+    //     return ;
+    // }
     try {
         const response = fetch(goUrl, {
             method: "POST",
-            body: JSON.stringify({message: "" , data : input}), // the JSON which is sent to the backend must have the same format as the data sent from the backend 
+            body: JSON.stringify({ message: "", data: input }), // the JSON which is sent to the backend must have the same format as the data sent from the backend 
             mode: 'no-cors',
             dataType: 'jsonp',
             headers: {
                 "Content-type": "application/json",
             }
         })
-        .then(res => res.json()) // res.json() converts the data of res which is coming from backend to js object
-        .then(content => {
-           // alert(content.data)
-           document.getElementById('goResult').classList.remove('hidden')
-           document.getElementById('goResult').classList.add('block')
-           document.getElementById('goInputMessage').innerHTML = content.message
-           document.getElementById('goInputStatus').innerHTML = content.data
+            .then(res => res.json()) // res.json() converts the data of res which is coming from backend to js object
+            .then(content => {
+                // alert(content.data)
+                document.getElementById('goDataResult').classList.add('hidden')
+                document.getElementById('goShaResult').classList.remove('hidden')
+                document.getElementById('goShaResult').classList.add('block')
+                document.getElementById('goShaMessage').innerHTML = content.message
+                document.getElementById('goShaStatus').innerHTML = content.data
+            })
+    } catch (error) {
+        console.error(`error: ${error.message}`)
+    }
+})
+
+gosh256Btn.addEventListener('click', () => {
+    let input = document.getElementById('goInputSh256').value;
+    let goUrl = `http://localhost:3061/go/sha256?sha=${input}`;
+    try {
+        const response = fetch(goUrl, {
+            method: "GET",
+            mode: 'no-cors',
+            //dataType: 'jsonp',
+            headers: {
+                "Content-type": "application/json",
+            }
         })
+            .then(res => res.json()) // res.json() converts the data of res which is coming from backend to js object
+            .then(content => {
+                // alert(content.data)
+                document.getElementById('goShaResult').classList.add('hidden')
+                document.getElementById('goDataResult').classList.remove('hidden')
+                document.getElementById('goDataResult').classList.add('block')
+                document.getElementById('goDataMessage').innerHTML = content.message
+                document.getElementById('goDataStatus').innerHTML = content.data
+            })
     } catch (error) {
         console.error(`error: ${error.message}`)
     }
--- golang/main.go
+++ golang/main.go
@@ -1,72 +1,107 @@
 package main
 
 import (
+	"context"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
-
+	"log"
 	"net/http"
-	//"strconv"
+
+	"github.com/redis/go-redis/v9"
 )
 
-type requestData struct {
-	Data string `json:"data"`
-}
-
 type responseData struct {
-	Data string `json:"data"`
+	Data    string `json:"data"`
 	Message string `json:"message"`
 }
 
-func goPost(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		b, err := io.ReadAll(r.Body) // reads byte by byte => [](key:value)
-		if err == io.EOF {
-			http.Error(w, err.Error(), http.StatusUnprocessableEntity)
-			return
+func goHandler(client *redis.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "POST":
+			b, err := io.ReadAll(r.Body) // reads byte by byte => [](key:value)
+			if err == io.EOF {
+				http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+				return
+			}
+			var d responseData
+			err = json.Unmarshal([]byte(b), &d)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+				return
+			}
+			var result responseData = responseData{Data: "Less than 8 chars", Message: "Error"} //-------------
+			if len(d.Data) > 8 {
+				//fmt.Println(d.Data,d.Message)
+				cryptoSha256 := sha256.New()
+				cryptoSha256.Write([]byte(d.Data))
+				cr := cryptoSha256.Sum(nil)
+				result = responseData{Data: hex.EncodeToString(cr), Message: "Your sha256:"} //-------------using string(cr) did not work
+				// client := redis.NewClient(&redis.Options{
+				// 	Addr:     "localhost:6379",
+				// 	Password: "", // no password set
+				// 	DB:       0,  // use default DB
+				// })
+				ctx := context.Background()
+				err := client.Set(ctx, result.Data, d.Data, 0).Err()
+				if err != nil {
+					panic(err)
+				}
+			}
+			jsonData, err := json.Marshal(result)
+			if err != nil {
+				http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+				return
+			}
+			log.Printf("%+v\n", result)
+			w.Header().Set("Content-Type", "application/json")
+
+			w.Write(jsonData)
+			break
+		case "GET":
+			data := r.URL.Query().Get("sha")
+			ctx := context.Background()
+			data, err := client.Get(ctx, data).Result()
+			result := responseData{Message: "Error", Data: "The sha does not exist"}
+			if err == nil {
+				result = responseData{Message: "Data:", Data: data}
+			}
+			jsonData, err := json.Marshal(result)
+			if err != nil {
+				http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+				return
+			}
+			log.Printf("%+v\n", result)
+			w.Header().Set("Content-Type", "application/json")
+
+			w.Write(jsonData)
+			break
+			
 		}
-		var d responseData
-		err = json.Unmarshal([]byte(b), &d)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusUnprocessableEntity)
-			return
-		}
-		var result responseData = responseData{Data: "Less than 8 chars" , Message: "Error"} //-------------
-		if len(d.Data) > 8  {
-			fmt.Println(d.Data,d.Message)
-			cryptoSha256 := sha256.New()
-			cryptoSha256.Write([]byte(d.Data))
-			cr := cryptoSha256.Sum(nil)
-			result = responseData{Data: hex.EncodeToString(cr), Message: "Your sha256:"} //-------------using string(cr) did not work
-		}
-		jsonData, err := json.Marshal(result)
-		if err != nil {
-			http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
-			return
-		}
-		fmt.Println(result)
-		w.Header().Set("Content-Type", "application/json")
-	
-		w.Write(jsonData)
+
 	}
+}
 
-}  
+func run(domain string, port int) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
-func run(port string)  {
-	
 	mux := http.NewServeMux()
 	handler := http.FileServer(http.Dir("../frontend/dist"))
-	mux.Handle("/", handler)                                          
+	mux.Handle("/", handler)
 	mux.Handle("/img/", http.FileServer(http.Dir("../frontend")))
 
-	mux.HandleFunc("/go/sha256", goPost)
-	
-	fmt.Print("Server is listening on port",port," ...\n")
+	mux.HandleFunc("/go/sha256", goHandler(client))
 
-	err := http.ListenAndServe(port, mux)
+	fmt.Printf("Server is listening on %s:%d\n", domain, port)
+
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", domain, port), mux)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -74,5 +109,5 @@ func run(port string)  {
 }
 
 func main() {
-	run(":3061");
+	run("localhost", 3061)
 }
