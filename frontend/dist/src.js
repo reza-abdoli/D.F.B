dbdad28b574aa13d4f9e2e57160c7a4d9e5851cb
reza <rabdoli2002@gmail.com>
2023-12-22 02:02:17 +0330

    front(80%)

--- frontend/dist/src.js
+++ frontend/dist/src.js
@@ -18,20 +18,36 @@ const dataPanel = document.getElementById('dataPanel')
 const floatPanelContainer = document.getElementById('floatPanelContainer')
 const floatPanel = document.getElementById('floatPanel')
 
-const rightPanel = document.getElementById('xx')
-const leftPanel = document.getElementById('leftPanel')
+const rightPanel = document.getElementById('rPanel')
+const leftPanel = document.getElementById('lPanel')
 
-
+const leftForm = document.getElementById('lForm')
+const rightForm = document.getElementById('rForm')
 
 shaPanel.addEventListener('click', function () {
    floatPanelContainer.classList.add('-translate-x-full')
    floatPanel.classList.add('translate-x-[50%]')
-   leftPanel.classList.add('translate-x-[30%]')
-   rightPanel.classList.add('translate-x-0')
+   leftPanel.classList.add('translate-x-0')
+   rightPanel.classList.remove('translate-x-0')
+   rightPanel.classList.add('translate-x-[30%]')
+
+   leftForm.classList.add('translate-x-full')
+   rightForm.classList.add('translate-x-full')
+
+   rightForm.classList.remove('opacity-0')
+  rightForm.classList.remove('z-10')
+   rightForm.classList.add('opacity-100')
+   rightForm.classList.add('z-50')
+   rightForm.classList.add('animate-show')
 })
+// if we wanna replace a property with a new value it's better to remove the old value and add the new value
 dataPanel.addEventListener('click', function () {
    floatPanelContainer.classList.remove('-translate-x-full')
    floatPanel.classList.remove('translate-x-[50%]')
-   leftPanel.classList.remove('translate-x-[30%]')
-   rightPanel.classList.remove('translate-x-0')
+   leftPanel.classList.remove('translate-x-0')
+   rightPanel.classList.remove('translate-x-[30%]')
+
+   leftForm.classList.remove('translate-x-full')
+
+
 })
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,12 +5,22 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'body-img': "url('./img/img2.webp')" // addressing from the output.css file
+        'body-img': "url('./img/img2.webp')", // addressing from the output.css file
+        'github-icon': "url('./img/github.png')"
       },
       fontFamily: {
        
         'montserrat': ['Montserrat', 'sans-serif']
       },
+      keyframes: {
+        formAnimation: {
+          '0%,49.99%': {  opacity: 0, 'z-index': 10 },
+          '50%,100%': { opacity: 1, 'z-index': 50},
+      }
+      },
+      animation: {
+        'show': 'formAnimation 1s'
+      }
     },
   },
   plugins: [],
